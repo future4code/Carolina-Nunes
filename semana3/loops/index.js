15b27e3a5d3440dcf22a51cf8a247bdb8f79af8d
Carolina Della Nina <carolirn@gmail.com>
2020-07-30 16:29:25 -0300

    loops

---
+++ semana3/loops/index.js
@@ -0,0 +1,58 @@
+// Exercícios de interpretação //
+// Exercício 1 - Será impresso:
+// O código está condicionando uma continuação que está atrelada a um número que é incrementado 
+// o resultado impresso é 10
+
+// let valor = 0
+// for(let i = 0; i < 5; i++) {
+//   valor += i
+// }
+// console.log(valor)
+
+// Exercício 2 - Será impresso:
+
+// const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+// for (let numero of lista) {
+//     console.log(numero)
+// }
+// a. Foram impressos os números maiores que 18.
+// b. Sim. É só retirar a condição if (numero > 18)
+
+// Desafio 1
+
+// const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+// let quantidadeAtual = 0
+// while(quantidadeAtual < quantidadeTotal){
+//   let linha = ""
+//   for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+//     linha += "0"
+//   }
+//   console.log(linha)
+//   quantidadeAtual++
+// }
+/* O resultado impresso é:
+0
+00
+000
+0000 */
+
+//Exercícios de escrita //
+//Exercício 3//
+/* a. e b.
+const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+
+for(let i = 0; i < 12; i++) {
+    const elemento = array[i]
+    console.log(elemento)
+    
+    console.log(elemento/10)
+} */
+
+//c. 
+const novoArray =[]
+
+//d. 
+
+
+//e. 
+
