ca32495bc657682bf27b4f42c2dec4a29151358e
Carolina Della Nina <carolirn@gmail.com>
2020-08-08 12:06:32 -0300

    unidade1 e unidade2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -17,7 +17,7 @@ Quantidade total de números 14
 /* A função compara os numeros de uma lista.
 Se o número na array for menor que o infinito, então numero1 é igual ao número da lista. 
 Se o número na array for maior que 0, então numero2 é igual ao numero da lista.
-Serão impressas todos os números com exceção do -10 */
+Serão impressos os números 1 e 1590 */
 
 // EXERCÍCIOS DE LÓGICA DE PROGRAMAÇÃO
 // EXERCÍCIO 1
@@ -49,11 +49,73 @@ d. true
 e. true */
 
 // EXERCÍCIO 3
-const quantidadeDeNumerosPares = true
-let i = 0
+// const quantidadeDeNumerosPares = 2
+// let i = 0
 
-while(i <= quantidadeDeNumerosPares) {
-    console.log(i*2)
-    i++
+// while(i <= quantidadeDeNumerosPares) {
+//     console.log(i*2)
+//     i++
+// }
+// O código não funciona inicialmente porque está incompleto, faltava o incremento.
+
+// EXERCÍCIO 4 - INCOMPLETO
+
+// const ladoA = true
+// const ladoB = true
+// const ladoC = true
+
+// let resultado = ladoA && ladoB && ladoC
+// console.log("O triângulo é equilátero", resultado)
+
+// resultado = bool1 && bool2 && bool3
+// console.log("b. ", resultado)
+
+// resultado = !resultado && (bool1 || bool1)
+// console.log("c. ", resultado)
+
+// console.log("e. ", typeof resultado)
+
+
+// if (ladoA === ladoB === ladoC) {
+//     console.log("O triângulo é equilátero")
+// } else if (ladoA === ladoB !== ladoC) {
+//     console.log("O triângulo é isósceles") 
+// } else {
+//     console.log("O triângulo é escaleno")
+// }
+
+// EXERCÍCIO 5 - letra B errada
+
+// a.
+const num1 = 15
+const num2 = 30
+
+if (num1 > num2) {
+    console.log(`O maior é ${num1}`)
+} else {
+    console.log(`O maior é ${num2}`)
 }
-/* O código não funciona inicialmente porque está incompleto.
+
+// b. 
+const condicao1 = (num1 % num2 > 0)
+const condicao2 = (num2 % num1 > 0)
+
+if (condicao1 === true) {
+    console.log(`${num1} é divisível por ${num2}`)
+} else if (condicao1 === false){
+    console.log(`${num1} não é divisível por ${num2}`)
+} else if (condicao2 === true) {
+    console.log(`${num2} é divisível por ${num1}`)
+} else (condicao2 === false)
+    console.log(`${num2} não é divisível por ${num1}`)
+
+
+// c.
+const resultado1 = num1 - num2
+const resultado2 = num2 - num1
+
+if (resultado1 > 0){
+    console.log(`A diferença entre eles é ${resultado1}`)
+} else (resultado2 > 0) 
+    console.log(`A diferença entre eles é ${resultado2}`)
+
