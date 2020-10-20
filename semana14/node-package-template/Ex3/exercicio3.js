f3659df82da4c722dd29a60b5fe2303462792017
Carolina Della Nina <carolirn@gmail.com>
2020-10-20 08:56:43 -0300

    ajuste exercicio

--- semana14/node-package-template/Ex3/exercicio3.js
+++ semana14/node-package-template/Ex3/exercicio3.js
@@ -1,9 +1,9 @@
 const task = process.argv[2]
-const taskList = ["Estudar"]
+const taskList = ["Estudar", "Cozinhar", "Lavar roupa"]
 
-const showTasks = (task) => {
-    taskList.push(...taskList, task)
+const showTasks = (newTask) => {
+    taskList.push(newTask)
 
-    return console.log(`Tarefa adicionada com sucesso!\n\n${taskList}`)
+    return console.log(`Tarefa adicionada com sucesso! Tarefas: ${taskList}`)
 }
 showTasks(task)
