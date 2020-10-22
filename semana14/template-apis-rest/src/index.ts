c878b01ff5fd9b154278bb86f2b6ffe0dd065755
Carolina Della Nina <carolirn@gmail.com>
2020-10-22 15:55:27 -0300

    aula43 incompleto

--- semana14/template-apis-rest/src/index.ts
+++ semana14/template-apis-rest/src/index.ts
@@ -10,51 +10,98 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+//Exercício 1
+// a. Usaria o método GET
+// b. A entidade é /users
 
-let users = [
+//Exercício2
+// a. Usando um filtro
+// b. Usando o TYPE e o ENUM
+
+//Exercício 4
+// a. É preciso substituir o dado existente, então usamos o index.
+// b. Não, porque ele só edita os dados um usuário existente, não cria outros.
+
+enum UserType {
+    ADMIN = "ADMIN",
+    NORMAL = "NORMAL"
+}
+
+type user = {
+    id: number,
+    name: string,
+    email: string,
+    type: UserType,
+    age: number
+}
+
+let users: user[] = [
     {
         id: 1,
         name: "Alice",
         email: "alice@email.com",
-        type: "ADMIN",
+        type: UserType.ADMIN,
         age: 12
     },
     {
         id: 2,
         name: "Bob",
         email: "bob@email.com",
-        type: "NORMAL",
+        type: UserType.NORMAL,
         age: 36
     },
     {
         id: 3,
         name: "Coragem",
         email: "coragem@email.com",
-        type: "NORMAL",
+        type: UserType.NORMAL,
         age: 21
     },
     {
         id: 4,
         name: "Dory",
         email: "dory@email.com",
-        type: "NORMAL",
+        type: UserType.NORMAL,
         age: 17
     },
     {
         id: 5,
         name: "Elsa",
         email: "elsa@email.com",
-        type: "ADMIN",
+        type: UserType.ADMIN,
         age: 17
     },
     {
         id: 6,
         name: "Fred",
         email: "fred@email.com",
-        type: "ADMIN",
+        type: UserType.ADMIN,
         age: 60
     }
 ]
+
+//Exercício 3
+
+app.get("/users/query", (req: Request, res: Response): void =>{
+
+    const name = req.query.name;
+    console.log("o nome que chegou: ", name);
+    try{
+
+        const user = users.filter((u)=> u.name === name);
+
+        if(!user){
+            throw new Error();
+        }
+
+        res.status(200).send(user);
+    }catch(error){
+        res.status(400).send({
+            message: "Error searching for users"
+        });
+    }
+});
+
 
 app.get("/users", (req: Request, res: Response): void =>{
 
@@ -100,6 +147,51 @@ app.post("/users", (req: Request, res: Response): void=>{
     }
 })
 
+
+
+app.put("/users", (req: Request, res: Response): void=>{
+
+    try{
+
+        const {id, name, email, age, type} = req.body;
+
+        const userIndex = users.findIndex((u)=> u.id === id);
+
+        if(userIndex === -1){
+            throw new Error();
+        }
+
+        users[userIndex].name = name;
+        users[userIndex].email = email;
+        users[userIndex].age = age;
+        users[userIndex].type = type;
+
+        res.status(200).send({message: "User updated successfully"});
+    }catch(error){
+        res.status(400).send({
+            message: "Error inserting users"
+        });
+    }
+});
+
+app.delete("/users/:id", (req: Request, res: Response): void =>{
+
+    try{
+        const result: number | undefined = users.findIndex(
+            (user) => user.id === Number(req.params.id)
+        )
+            if(result === -1){
+                throw new Error
+            }
+        users.splice(result, 1)
+        
+        res.status(200).end()
+    }catch(error){
+        res.status(400).send({
+            message: "Error searching for users"
+        });
+    }
+});
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
        const address = server.address() as AddressInfo;
