80c03586d0648677a59ce5ccff175234d91e7ed5
Carolina Della Nina <carolirn@gmail.com>
2020-10-27 11:43:25 -0300

    pendencias

--- semana11/projeto/projeto-semana11/src/App.test.js
+++ semana11/projeto/projeto-semana11/src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe("Funcionalidade adicionar tarefa", () => {
+  test("tarefa é incluída no local", () => {
+    const { getByText } = render(<App />);
+
+    const addButton = getByText(/CRIAR TAREFA/i);
+
+    fireEvent.click(addButton);
+
+    const textoPlanner = getByText(/Planner/);
+
+    expect(textoPlanner).toHaveTextContent("");
+  });
 });
